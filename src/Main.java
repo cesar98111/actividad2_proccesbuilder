97b3d3bda7726a25eb7d023b03010c240fa8f066
cesar jesus leon martin <cesilius98@gmail.com>
2022-10-18 18:16:17 +0000

    Feature: desarrollo inicial de las ventanas

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,24 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command("firefox");
 
+        Process proceso [] = new Process[sc.nextInt()];
+
+        try{
+            for (int i = 0; i < proceso.length; i++) {
+                proceso[i] = pb.start();
+            }
+
+            for (int i = 0; i < proceso.length; i++) {
+                proceso[i].waitFor();
+            }
+        }catch (IOException | InterruptedException e){
+            System.out.println("no funciona");
+        }
+
+    }
 }
