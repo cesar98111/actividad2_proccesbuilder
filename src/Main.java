872abe1009f190ee68d66a7ca7903a6bfeb59f36
cesar jesus leon martin <cesilius98@gmail.com>
2022-10-18 18:22:58 +0000

    Feature: implementada duracion de las paginas abiertas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,7 +16,8 @@ public class Main {
             }
 
             for (int i = 0; i < proceso.length; i++) {
-                proceso[i].waitFor();
+                proceso[i].waitFor(100, TimeUnit.MILLISECONDS);
+                proceso[i].destroy();
             }
         }catch (IOException | InterruptedException e){
             System.out.println("no funciona");
